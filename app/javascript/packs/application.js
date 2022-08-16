fd194ac981f8e2e9e6166ec182901c28323c67a0
Takahiro Iizuka <agdjgt@gmail.com>
2022-08-16 16:50:20 +0900

    Added bulma css and style to application.html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,19 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBergers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  $navbarBergers.forEach( el => {
+    el.addEventListener('click', () => {
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      el.classList.toggle('is-active')
+      $target.classList.toggle('is-active')
+    });
+  });
+});
